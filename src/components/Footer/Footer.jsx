37cf694f901d5fa7a592acdf8ed2fe1299827657
Diamela Lares <102709669+diamecjs@users.noreply.github.com>
2024-05-21 12:37:07 -0500

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className={styles["container-footer"]}>
             <p className={styles["primer-p"]}>📧 Mail</p> <p className={styles["segundo-p"]}> diame.cjs@gmail.com</p>
             <p className={styles["primer-p"]}>📱 Telefono</p> <p  className={styles["segundo-p"]}> 🇨🇴 +57 321 586 46 22  - 🇺🇾 +598 96 218 481</p>
-            <p className={styles["primer-p"]}>📍 Ubicación </p><p  className={styles["segundo-p"]}> Piriápolis - Uruguay</p>
+            <p className={styles["primer-p"]}>📍 Ubicación </p><p  className={styles["segundo-p"]}> Santa Marta - Colombia</p>
 
         </div>
     );
